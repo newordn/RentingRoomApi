73f05effe3f66ecd963bc6d2c060fd52e292816e
neword <newordn@gmail.com>
2019-11-08 14:07:33 +0100

    Added connexion and inscription functionnality to the api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const {GraphQLServer} = require('graphql-yoga')
 const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const {prisma} = require('./prisma/generated/prisma-client')
+
 const resolvers = {
-Query
+Query,
+Mutation
 }
 
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: {prisma}
 })
 
 server.start(()=>console.log(`Server is running on localhost:4000`))
