89845e94849eb8f096f9eacdb76c39da20c31633
neword <newordn@gmail.com>
2019-11-12 10:24:39 +0100

    Added chamber detail

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,10 +2,12 @@
     function chambers(parent,args,context){
         return context.prisma.chambers()
     }
-    
+    function chamber(parent,args,context){
+        return context.prisma.chamber({id:args.id})
+    }
     function users(parent,args,context){
         return context.prisma.users()
     }
     module.exports={
-        chambers,users
+        chambers,users,chamber
     }
